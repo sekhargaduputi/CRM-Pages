6c68e4addaf223f1a8c9cf9f24f1d304afc53265
sekhar <gaduputisekhar87@gmail.com>
2019-05-22 15:01:32 +0530

    add program

---
+++ src/Examples1/ReverseString.java
@@ -0,0 +1,14 @@
+package Examples1;
+
+public class ReverseString {
+	public static void main(String args[]){
+		String s="Selenium";
+		
+		for(int i=s.length()-1;i>=0;i--){
+			
+			System.out.print(s.charAt(i));
+		}
+		
+	}
+
+}
